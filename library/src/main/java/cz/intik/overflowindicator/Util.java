f1503af439a9134609a26a816575b724b8aeb5f0
Petr Introvic <introvic.petr@gmail.com>
2019-08-30 13:33:18 +0200

    Update libraries, convert to Kotlin

--- library/src/main/java/cz/intik/overflowindicator/Util.java
+++
@@ -1,9 +0,0 @@
-package cz.intik.overflowindicator;
-
-import android.content.res.Resources;
-
-class Util {
-    static int dpToPx(double dp) {
-        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
-    }
-}
